bd91aa98289e8cc628ad4c6bc497362698a9b639
RobertoDF <roberto.defilippo@outlook.com>
2022-07-29 13:25:09 +0200

    update funding and institutions

--- Manuscript/De_Filippo_et_al_2022.py
+++ Manuscript/De_Filippo_et_al_2022.py
@@ -11,13 +11,23 @@ from Utils.Settings import manuscript_folder
 doc = DocxTemplate(f"{manuscript_folder}/Manuscript_template.docx")
 
 title = "Differential ripple propagation along the hippocampal longitudinal axis"
-authors = "Roberto De Filippo¹ and Dietmar Schmitz¹"
-affiliations = "¹ Charité Universitätsmedizin Berlin, corporate member of Freie Universität Berlin, Humboldt-Universität" \
-               " zu Berlin,and Berlin Institute of Health; Neuroscience Research Center, 10117 Berlin, Germany"
+authors = "Roberto De Filippo¹ and Dietmar Schmitz¹²³⁴⁵"
+affiliations =  "¹ Charité Universitätsmedizin Berlin, corporate member of Freie Universität Berlin, Humboldt-Universität" \
+                " zu Berlin,and Berlin Institute of Health; Neuroscience Research Center, 10117 Berlin, Germany. \n" \
+                "² German Center for Neurodegenerative Diseases (DZNE) Berlin, 10117 Berlin, Germany. \n"\
+                "³ Charité-Universitätsmedizin Berlin, corporate member of Freie Universität Berlin, Humboldt-Universität Berlin, and "\
+                "Berlin Institute of Health, Einstein Center for Neuroscience, 10117 Berlin, Germany. \n"\
+                "⁴ Charité-Universitätsmedizin Berlin, corporate member of Freie Universität Berlin, Humboldt-Universität Berlin, " \
+                "and Berlin Institute of Health, NeuroCure Cluster of Excellence, 10117 Berlin, Germany. \n"\
+                "⁵ Humboldt-Universität zu Berlin, Bernstein Center for Computational Neuroscience, Philippstr. 13, 10115 Berlin, Germany. "\
+
+
 correspondence_to = "roberto.de-filippo@charite.de and dietmar.schmitz@charite.de"
 keywords = "Hippocampal ripples, Ripples propagation, Anisotropy"
 
-acknowledgements = "This work was supported by the Bundesministerium for Bildung und Forschung (SFB1315-327654276) grant.  " \
+acknowledgements = "This study was supported by the German Research Foundation Deutsche Forschungsgemeinschaft (DFG), " \
+                   "project 184695641 - SFB 958, project 327654276 - SFB 1315, Germany's Excellence Strategy - Exc-2049-390688087) and by the " \
+                   "European Research Council (ERC) under the European Union's Horizon 2020 research and innovation programme (Grant agreement No. 810580). " \
                 "We thank J.T. Tukker, N. Maier for feedback on an early version of the manuscript and the members of the Schmitz lab for " \
                    "scientific discussion. We thank Willy Schiegel and Tiziano Zito for technical expertise in cluster computing. We thank Federico Claudi " \
                    "for support with brainrender. " \
