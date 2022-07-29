1a51b6704c2e5ecdf4cb5dbdf0efdd4b96e97ae6
RobertoDF <roberto.defilippo@outlook.com>
2022-07-29 13:29:09 +0200

    update funding and institutions

--- Manuscript/abstract.py
+++ Manuscript/abstract.py
@@ -9,10 +9,10 @@ with open(f'{output_folder_calculations}/clean_ripples_calculations.pkl', 'rb') 
 abstract = "Hippocampal ripples are highly synchronous neural events critical for memory consolidation and retrieval. " \
            "A minority of strong ripples has been shown to be of particular importance in situations of increased memory demands. " \
            "The propagation dynamics of strong ripples inside the hippocampal formation are however still opaque. " \
-           f"Using an open access database of large-scale electrophysiological recordings in {len(ripples_calcs)} " \
-           f"mice we extensively analyzed ripple propagation in the septal half of the hippocampal formation. " \
-           "Strong ripples propagate differentially in the septal and temporal direction along the hippocampal longitudinal axis, " \
-           "the hippocampus is therefore anisotropic in relation to ripple propagation. Hippocampal anisotropy is explained by " \
+           f"We performed an extensive analysis of ripple propagation within the septal half of the hippocampal formation. " \
+           "Unexpectedly, strong ripples propagate differentially in the septal and temporal direction along the hippocampal longitudinal axis, " \
+           "Thus, the hippocampus is anisotropic in relation to ripple propagation. Hippocampal anisotropy is explained by " \
            "the ability of the septal hippocampal pole to generate" \
            " longer strong ripples that engage more neurons" \
-           " and elicit spiking activity for an extended time along the entire septal half of the hippocampal formation."
+           " and elicit spiking activity for an extended time along the entire septal half of the hippocampal formation. " \
+           "Our results suggest a possible distinctive role of the hippocampal septal pole in conditions of high memory demands. "
