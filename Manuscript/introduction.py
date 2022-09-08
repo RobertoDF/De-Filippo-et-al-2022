084ac9d47d9dd9aba92be7af8200cda78fb31bfa
RobertoDF <roberto.defilippo@outlook.com>
2022-09-08 18:29:26 +0200

    update

--- Manuscript/introduction.py
+++ Manuscript/introduction.py
@@ -3,23 +3,34 @@ introduction= "Hippocampal ripples are brief oscillatory events detected in the 
               " hippocampal subregions {Hulse, 2016 #1141; Ylinen, 1995 #1140}. " \
               "An higher ripple incidence during memory encoding is associated with superior recall performance {Norman, 2019 #1142}, " \
               " furthermore, ripple incidence is increased during successful memory retrieval {Vaz, 2019 #1147}." \
-              "Ripples are also important for memory consolidation both in awake and sleep conditions {Jadhav, 2012 #1143; Roux, 2017 #1144; Sirota, 2003 #1145; Girardeau, 2009 #1146}," \
-             "Accordingly, ripples are considered to play a crucial role in memory processes and reorganization of memory engrams {Girardeau, 2011 #1148; Buzsáki, 2015 #1110;Diba, 2007 #1111;" \
-             "Foster, 2006 #1112;Xu, 2019 #1113;Takahashi, 2015 #1114;Davidson, 2009 #1116;Pfeiffer, 2015 #1117;" \
-             "Dragoi, 2011 #1118;Girardeau, 2009 #1120}. " \
-              "Ripples duration follows a skewed distribution with a minority of long-duration ripples, interestingly, the fraction of long-duration ripples " \
-              "is increased in novel contexts, in tasks requiring memory and during correct recalls {Fernández-Ruiz, 2019 #1121}. These reults point at a specific role " \
-              "of long-duration ripples in situations of high mnemonic demand" \
-             " Reducing ripple duration artificially causes degraded working memory performance {Jadhav, 2012 #1135}, on the contrary, " \
-             "prolongation has a beneficial effect {Fernández-Ruiz, 2019 #1121}. These results are consistent with a possible " \
-             "power law distribution of ripples where a minority of stronger ripples is responsible for a substantial part of memory requirements. " \
-             "Indeed, ripple duration is directly proportional to ripple amplitude {Patel, 2013 #1133}. "\
-             "It is therefore of interest to identify the possible electro-physiological peculiarities of this sub-class of ripples. " \
-              "We focused our attention on propagation within the hippocampal formation. Connectivity with cortical and sub-cortical areas varies  " \
-              "along the longitudinal axis of the hippocampus {Moser, 1998 #1149; Vogel, 2020 #1151; Fanselow, 2010 #1150}, the different connectivity contributes " \
-              "to explain the functional organization gradient between a predominantly spatio-visual (septal pole) or emotional (temporal pole) processing." \
-             "The dataset provided by the Allen Institute {Siegle, 2021 #1127} enabled us to study comprehensively ripples features " \
-              "across the dorsal half of the hippocampus. The size of this dataset made it possible to unveil propagation details that could have been missed " \
-              "in previous studies."\
+              "Ripples are also involved in memory consolidation both in awake and sleep conditions {Jadhav, 2012 #1143; Roux, 2017 #1144; Sirota, 2003 #1145; Girardeau, 2009 #1146}, " \
+              "disrupting awake ripples during learning causes a persisting performance degradation, the same effect can be achieved by " \
+              "silencing ripples during post-learning sleep. " \
+              "Accordingly, ripples are considered to play a crucial role in memory processes and reorganization of memory engrams {Girardeau, 2011 #1148; Buzsáki, 2015 #1110;Diba, 2007 #1111;" \
+              "Foster, 2006 #1112; Xu, 2019 #1113;Takahashi, 2015 #1114;Davidson, 2009 #1116; Pfeiffer, 2015 #1117;" \
+              "Dragoi, 2011 #1118; Girardeau, 2009 #1120}. " \
+              "Ripples duration exhibits a skewed distribution with only a minority of long-duration ripples (>100 ms). Interestingly, the fraction of long-duration ripples " \
+              "is increased in novel contexts, in tasks memory-demanding and during correct recalls {Fernández-Ruiz, 2019 #1121}. " \
+              " Reducing ripple duration artificially causes degraded working memory performance {Jadhav, 2012 #1135}, on the contrary, " \
+              "prolongation has a beneficial effect {Fernández-Ruiz, 2019 #1121}. " \
+              "Ripple duration is directly proportional to ripple amplitude {Patel, 2013 #1133}, therefore, " \
+              "these results point at a specific role " \
+              "of long-duration/stronger ripples in situations of high mnemonic demand and are consistent with a possible " \
+              "power law distribution where a minority of ripples" \
+              " is responsible for a substantial part of memory requirements. " \
+              "It is therefore of interest to identify the possible electro-physiological peculiarities of this ripples subgroup. Do strong ripples propagate differently " \
+              "compared to common ripples? Are strong ripples generated homogeneously along the hippocampal longitudinal axis? Do ripples have a preferred longitudinal " \
+              "directionality? " \
+              "In this study we focused our attention on ripples generation and propagation within the hippocampal formation. Connectivity with cortical and sub-cortical areas varies  " \
+              "along the longitudinal axis of the hippocampus {Moser, 1998 #1149; Vogel, 2020 #1151; Fanselow, 2010 #1150} and the different connectivity contributes " \
+              "to explain the functional organization gradient between a predominantly spatio-visual (septal pole) and emotional (temporal pole) processing. " \
+              "Ripples generated in the septal and temporal hippocampal pole has already been shown to be temporally independent and able to engage " \
+              "different networks downstream {Sosa, 2020 #1154}. Consequentially, an inhomogeneous generation chance along the longitudinal axis " \
+              "might have an impact on the identity of the neurons contacted downstream by different ripples subgroups." \
+              "Our work is based on the dataset provided by the Allen Institute {Siegle, 2021 #1127}, this dataset enabled us to study" \
+              " comprehensively ripples features " \
+              "across the septal half of the hippocampus. Previous studies have looked at ripple propagation along the longitudinal " \
+              "axes of the hippocampus {Patel, 2013 #1133; Kumar, 2020 #1153}" \
+              ", however, the size of this dataset made it possible to unveil propagation details previously overlooked."\
 
 
