5a7cca527029f8486f81505ed6c23ecea785e580
RobertoDF <roberto.defilippo@outlook.com>
2022-09-14 18:52:43 +0200

    save

--- Manuscript/abstract.py
+++ Manuscript/abstract.py
@@ -12,11 +12,11 @@ abstract = "Hippocampal ripples are highly synchronous neural events critical fo
            "The propagation dynamics of strong ripples inside the hippocampal formation are however still opaque. " \
            f"We analyzed extensively ripple propagation within the septal half of the hippocampal formation in an open access dataset" \
            f" provided by the Allen Institute. " \
-           "Surprisingly, strong ripples propagate differentially in the septal and temporal direction along the hippocampal longitudinal axis. The majority " \
-           "of strong ripples is always generated locally, however, the septal hippocampal pole is able to generate" \
+           "Surprisingly, strong ripples propagate differentially in the septal and temporal direction along the hippocampal longitudinal axis. " \
+           "Most strong ripples are always generated locally, however, the septal hippocampal pole is able to generate" \
            " longer ripples that engage more neurons" \
-           " and elicit spiking activity for an extended time along the entire septal half of the hippocampal formation. Septally-generated " \
-           "ripples have therefore higher chances of retaining their strength while travelling within the hippocampus. " \
-           f"A substantial portion of the variance in strong ripple duration (R² = {r_strong}) is explained solely by the ripple starting " \
-           f"position on the longitudinal axis.  " \
-           "Our results suggest a possible distinctive role of the hippocampal septal pole in conditions of high memory demands. "
+           " and elicit spiking activity for an extended time along the entire septal half of the hippocampal formation. Ripples generated septally " \
+           "have therefore higher chances of retaining their strength while travelling within the hippocampus. " \
+           f"A substantial portion of the variance in strong ripple duration (R² = {r_strong}) is explained solely by the ripple generation " \
+           f"point on the longitudinal axis.  " \
+           "Our results suggest a possible distinctive role of the hippocampal septal pole in conditions of high memory demand. "
