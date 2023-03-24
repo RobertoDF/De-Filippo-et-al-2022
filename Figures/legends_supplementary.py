db0104e2136404951449d4a2409b787f1b897f43
RobertoDF <roberto.defilippo@outlook.com>
2023-03-24 13:20:40 +0100

    supplementary figures legends to Elife style

--- Figures/legends_supplementary.py
+++ Figures/legends_supplementary.py
@@ -3,17 +3,17 @@ from Utils.Settings import Adapt_for_Nature_style
 from Utils.Utils import Naturize_text
 
 legends_supplementary = {
-    "Supplementary Figure 1. Spatial coordinates of all recorded brain regions.": "2D histograms (upper diagonal), "
+    "Figure 1 - Figure supplement 1. Spatial coordinates of all recorded brain regions.": "2D histograms (upper diagonal), "
     "scatter plots (lower diagonal) and kernel density estimate plots (diagonal) of all the recorded regions color-coded according "
     "to the Allen Institute color scheme. HPF=hippocampus, TH=thalamus, HY=hypothalamus and MB=midbrain. M-L axis is zeroed at the midline.",
 
-    "Supplementary Figure 2. Correlation between ripple duration and strength per session.": "Red line represents linear regression with confidence interval of 95% estimated via bootstrap. "
+    "Figure 1 - Figure supplement 2. Correlation between ripple duration and strength per session.": "Red line represents linear regression with confidence interval of 95% estimated via bootstrap. "
                                                                                   "*** means p < 0.0005.",
 
-    "Supplementary Figure 3. Comparison between correlation of ripple strength and duration with underlying spiking.": "Ripple strength correlates significantly better with "
+    "Figure 1 - Figure supplement 3. Comparison between correlation of ripple strength and duration with underlying spiking.": "Ripple strength correlates significantly better with "
                                                                                                                        "the underlying ripple spiking activity. * means p < 0.0005. ",
 
-    "Supplementary Figure 4. Ripple-associated LFP responses are predominantly observed in hippocampal structures.":
+    "Figure 1 - Figure supplement 4. Ripple-associated LFP responses are predominantly observed in hippocampal structures.":
    f"(A) Rendering of probe locatiosn for session {session_id_supp_fig1}. "
    f"(B) First column: Raw LFP traces color coded according to probe identity, "
    f"superimposed in black the trace after high-pass filtering to show the presence of a ripple. Scale bar: 250 µV. "
@@ -30,12 +30,12 @@ legends_supplementary = {
    f"(F) Violin plots showing the distribution of ∫Ripple and RIVD z-scored per session,"
    f" hippocampal regions (text in green) show the biggest responses.",
 
-    "Supplementary Figure 5. Hippocampal sections.": "(A) Variance explained between 3D distances and distance on each spatial axis across CA1 recording locations. "
+    "Figure 1 - Figure supplement 5. Hippocampal sections.": "(A) Variance explained between 3D distances and distance on each spatial axis across CA1 recording locations. "
                                                      "(B) Histogram showing the three sections across the M-L axis, the hippocampus was divided in order to have "
                                                       "an equal number of recordings in each section. "
                                                      "(C) Rendering of the 3 sections and associated recording locations (black dots).",
 
-   "Supplementary Figure 6. Spatio-temporal lag maps of locally and not locally generated ripples": ""
+   "Figure 3 - Figure supplement 1. Spatio-temporal lag maps of locally and not locally generated ripples": ""
              "Spatio-temporal profiles are symmetrical, strong indication of similar propagation speed regardless of seed position. (A) Recording locations relative to (B). Red circles represents the reference locations across all sessions "
                         f"(n sessions={trajectories_by_strength['Session'].unique().shape[0]}), black circles represents the remaining recording locations. "
                         f"(B) Left: Medio-lateral propagation of locally generated ripples (generated in the reference section), each line represents the average of one session. "
@@ -44,20 +44,20 @@ legends_supplementary = {
                          f"(C) Same as A. (D) Same as B. Reference locations are the most lateral per session. "
                         f"(E) Same as A. (F) Same as B. Reference locations are the most central per session. ",
 
-   "Supplementary Figure 7. Strength conservation in medially and laterally generated ripples.":
+   "Figure 3 - Figure supplement 2. Strength conservation in medially and laterally generated ripples.":
       "(A) Strength conservation index in strong ripples grouped by reference location. Ripples generated in the lateral section show"
       f"significantly lower strength conservation (p={round(pvalues_supp_4, 9)}, Student's t-test).  "
       "(B) Strength conservation index in common ripples grouped by reference location.",
 
-   "Supplementary Figure 8. Spatial location does not influence ∫Ripple.":
+   "Figure 3 - Figure supplement 3. Spatial location does not influence ∫Ripple.":
       "Relationship between Z-scored ∫Ripple (top row) or ∫Ripple (bottom row) and each spatial axis (M-L, A-P or D-V). Spatial location "
       f"has a negligible effect on ∫Ripple.",
 
-   "Supplementary Figure 9. Spatial location does not influence ripple amplitude.":
+   "Figure 3 - Figure supplement 4. Spatial location does not influence ripple amplitude.":
       "Relationship between Z-scored amplitude (top row) or amplitude (bottom row) and each spatial axis (M-L, A-P or D-V). Spatial location "
       f"has a negligible effect on ripple amplitude.",
 
-   "Supplementary Figure 10. ":
+   "Figure 4 - Figure supplement 1. Differential spiking of hippocampal neurons between different conditions.":
       "(A) Grand average of the differences between medial and lateral ripples induced spiking activity in "
       "putative excitatory (left) and inhibitory neurons (right). Putative excitatory and inhibitory neurons "
       "show similiar spiking patterns in lateral and medial ripples. "
@@ -69,7 +69,7 @@ legends_supplementary = {
       "lateral ripples."
     ,
 
-   "Supplementary Figure 11. Spiking rate and fraction of active neurons are significantly higher in medial ripples":
+   "Figure 4 - Figure supplement 2. Spiking rate and fraction of active neurons are significantly higher in medial ripples.":
       f"(A) Fraction of active neurons per ripple grouped by ripple seed location. (Medial seed={100*round(data_sup_9_fraction_clu.groupby('Location seed').mean()['Fraction active neurons per ripple (%)']['Medial seed'], 2)}±"
       f"{100*round(data_sup_9_fraction_clu.groupby('Location seed').sem()['Fraction active neurons per ripple (%)']['Medial seed'], 2)}%, lateral seed={100*round(data_sup_9_fraction_clu.groupby('Location seed').mean()['Fraction active neurons per ripple (%)']['Lateral seed'], 2)}±"
       f"{100*round(data_sup_9_fraction_clu.groupby('Location seed').sem()['Fraction active neurons per ripple (%)']['Lateral seed'], 2)}%, p-value={'{:.2e}'.format(ttest_clus_per_ripple['p-val'].values[0])}, Student's t-test). "
@@ -78,14 +78,14 @@ legends_supplementary = {
       f"{100*round(data_sup_9_spiking_rate.groupby('Location seed').sem()['Spiking rate per 10 ms']['Lateral seed'], 2)}%, p-value={'{:.2e}'.format(ttest_late_spiking['p-val'].values[0])}, Student's t-test). "
       f"Asterisks mean p < 0.05, Student's t-test.",
 
-   "Supplementary Figure 12. Spiking rate and fraction of active neurons are increased in the late phase post-ripple "
+   "Figure 4 - Figure supplement 3. Spiking rate and fraction of active neurons are increased in the late phase post-ripple."
    "start in medial ripples both in putative excitatory and inhibitory neurons.":
       "(A) Average spiking rate in early (left) and late (right) phase post-ripple start grouped by ripple seed location and putative neuron identity. "
       "Asterisks mean p < 0.05, ANOVA with pairwise Tukey post-hoc test. "
       "(B) Fraction of active neurons per ripple in early (left) and late (right) phase post-ripple start grouped by ripple "
       "seed location and putative neuron identity. Asterisks mean p < 0.05, ANOVA with pairwise Tukey post-hoc test.",
 
-"Supplementary Figure 13. Units features in medial and lateral sections":
+"Figure 4 - Figure supplement 4. Units features in medial and lateral sections.":
       f"(A) Kernel density estimate plot of waveform duration (p-value={'{:.2e}'.format(p_val_wav_dur_inh)}), firing rate (p-value={'{:.2e}'.format(p_val_fir_rate_inh)}), "
       f"waveform amplitude (p-value={'{:.2e}'.format(p_val_wav_amp_inh)}), waveform repolarization slope (p-value={'{:.2e}'.format(p_val_wav_repolarization_slope_inh)}), "
       f"waveform recovery slope (p-value={'{:.2e}'.format(p_val_wav_rec_slope_inh)}) and waveform peak-through ratio (p-value={'{:.2e}'.format(p_val_wav_PT_ratio_inh)}) grouped by hippocampal section."
@@ -103,7 +103,7 @@ legends_supplementary = {
       f"waveform recovery slope (p-value={'{:.2e}'.format(p_val_ks_wav_rec_slope_exc)}) and waveform peak-through ratio (p-value={'{:.2e}'.format(p_val_ks_wav_PT_ratio_exc)}) grouped by hippocampal section."
       "Asterisks mean p < 0.05, Kolgomorov-Smirnov test.",
 
-"Supplementary Figure 14. Spiking rate modulation in medial and lateral ripples across brain regions":
+"Figure 5 - Figure supplement 1. Spiking rate modulation in medial and lateral ripples across brain regions.":
     "(A) Relationship between baseline (120 ms before ripple start) and medial ripple (0-50 ms) firing rate for clusters recorded in  HPF, Isocortex, MB and TH. In the Isocortex and MB plot "
     "we excluded the minority of clusters showing modulation >50% in response to either lateral or medial ripples(grey dots). Dashed black line represents absence of any influence, "
            "dashed red line represents a 50% increased spiking rate. "
@@ -111,7 +111,7 @@ legends_supplementary = {
     "we excluded the minority of clusters showing modulation >50% in response to either lateral or medial ripples (grey dots). Dashed black line represents absence of any influence, "
            "dashed red line represents a 50% increased spiking rate. ",
 
-"Supplementary Figure 15. Ripple modulation across HPF, Isocortex, MB and TH":
+"Figure 5 - Figure supplement 2. Ripple modulation density histograms.":
 "(A) Left: Early (0-50 ms) ripple modulation of hippocampal clusters in response to lateral and medial ripples. Dashed black line represents absence of any influence, "
            "dashed red line represents a 50% increased spiking rate. Wilcoxon signed-rank test. Right: "
 "Late (50-120 ms) ripple modulation of hippocampal clusters in response to lateral and medial ripples. Dashed black line represents absence of any influence, "
@@ -119,17 +119,17 @@ legends_supplementary = {
 "(B) Ripple modulation of cortical (left), MB (middle) and TH (right) clusters in response to lateral and medial ripples. Dashed black line represents absence of any influence, "
            "dashed red line represents a 50% increased spiking rate. Wilcoxon signed-rank test",
 
-"Supplementary Figure 16. Cortical clusters showing ripple engagement":
+"Figure 5 - Figure supplement 3. Cortical clusters showing ripple engagement.":
     "In pink clusters showing medial ripples engagement (at least 25%), in purple clusters showing lateral ripples engagement (at least 25%) and in red clusters showing "
     "engagement (at least 25%) both in medial and lateral ripples.",
 
-"Supplementary Figure 17. Ripple modulation across HPF, Isocortex, MB and TH":
+"Figure 5 - Figure supplement 4. Ripple modulation across HPF, Isocortex, MB and TH.":
 "(A) Ripple modulation in response to lateral and medial ripples during the early  "
            "ripple phase in cortical (top), MB (middle) and TH (bottom) clusters. Wilcoxon signed-rank test or Student's t-test (if normality established). "
 "(B) Ripple modulation in response to lateral and medial ripples during the late  "
            "ripple phase in cortical (top), MB (middle) and TH (bottom) clusters. Wilcoxon signed-rank test or Student's t-test (if normality established). ",
 
-"Supplementary Figure 18. Pre-ripple modulation across HPF, Isocortex, MB and TH":
+"Figure 5 - Figure supplement 5. Pre-ripple modulation across HPF, Isocortex, MB and TH.":
 "(A) Pre-ripple modulation in response to lateral and medial ripples during the early  "
            "ripple phase in cortical clusters. Wilcoxon signed-rank test or Student's t-test (if normality established). "
 "(B) Ripple modulation in response to lateral and medial ripples during the late  "
@@ -137,7 +137,7 @@ legends_supplementary = {
 "(C) Ripple modulation in response to lateral and medial ripples during the late  "
            "ripple phase in TH clusters. Wilcoxon signed-rank test or Student's t-test (if normality established). ",
 
-"Supplementary Figure 19. Clusters preference in ripple engagement by hippocampal subfields.":
+"Figure 5 - Figure supplement 6. Clusters preference in ripple engagement by hippocampal subfields.":
     "Preference in ripple engagement in CA1, CA3, DG, ProS and SUB."
 }
 
